c78ab9994a0ad95b6a49ca3b28a1b3a9725ca5a6
zhangjunpeng <zhangjunpeng@nextmar.com>
2021-06-07 16:49:07 +0800

    create

---
+++ cmake-build-debug/CMakeFiles/opengl_demo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opengl_demo.dir/main.cpp.o"
+  "CMakeFiles/opengl_demo.dir/src/glad.c.o"
+  "opengl_demo"
+  "opengl_demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/opengl_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
